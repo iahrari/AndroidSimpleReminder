836e0956ed071a1ab3089189b1dcb8942bec2f20
Iman Ahrari <imi2000.ia@gmail.com>
2020-08-23 18:51:05 +0430

    Better support for dark mode and improved navigation

--- app/src/main/java/com/github/iahrari/reminder/ui/view/MainActivity.kt
+++ app/src/main/java/com/github/iahrari/reminder/ui/view/MainActivity.kt
@@ -3,17 +3,37 @@ package com.github.iahrari.reminder.ui.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.github.iahrari.reminder.R
 import com.github.iahrari.reminder.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    var listener: OnBackPressed? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setSupportActionBar(binding.toolbar)
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
+        val nav = findNavController(R.id.nav_host_fragment)
+        setupActionBarWithNavController(nav)
+    }
+
+    override fun onBackPressed() {
+        if (listener != null)
+            listener!!.onBackPressed()
+        else super.onBackPressed()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 
     fun setFloatingButtonAction(operation: () -> Unit){
@@ -22,12 +42,24 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun setToolbarTitle(title: Int){
-        TODO("Implement this method")
+    fun setToolbarTitle(title: Int?){
+        if (title != null)
+            binding.toolbarTitle.setText(title)
+        else binding.toolbarTitle.text = ""
     }
 
     fun setFloatingButtonUi(title: Int, icon: Int){
-        binding.floatingAction.setText(title)
-        binding.floatingAction.setIconResource(icon)
+        lifecycleScope.launch {
+            binding.floatingAction.shrink()
+            delay(250)
+            binding.floatingAction.setIconResource(icon)
+            delay(250)
+            binding.floatingAction.setText(title)
+            binding.floatingAction.extend()
+        }
+    }
+
+    interface OnBackPressed{
+        fun onBackPressed()
     }
 }
