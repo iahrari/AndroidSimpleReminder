f5f16dcdc7b202312461b937c731099cbd4bb8e5
Iman Ahrari <imi2000.ia@gmail.com>
2020-10-24 20:39:29 +0330

    added singleton for database

--- app/src/main/java/com/github/iahrari/reminder/di/DIModule.kt
+++ app/src/main/java/com/github/iahrari/reminder/di/DIModule.kt
@@ -7,11 +7,14 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
 
 
 @InstallIn(ApplicationComponent::class)
 @Module
 object DIModule {
+
+    @Singleton
     @Provides
     fun provideDatabase(@ApplicationContext context: Context): Database {
         return Database.getInstance(context)
